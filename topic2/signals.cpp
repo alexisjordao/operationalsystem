095808be487cdc0803f2ccbd61a85b3c5157ffd9
Renato Nascimento <rhnasc@atlassian.com>
2016-03-14 01:26:17 -0300

    Add answer to problemset #2

---
+++ topic2/signals.cpp
@@ -0,0 +1,98 @@
+/*
+ * Universidade Federal do Rio Grande do Norte
+ * Instituto Metropole Digital
+ *
+ * copyright: Ivanovitch Silva
+ * e-mail: ivan@imd.ufrn.br
+ *
+ * Tópico: tratando sinais no linux
+ * Programa: IPC através da função kill
+ */
+ 
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <csignal>
+
+// Identificador do processo
+pid_t idProcesso;
+
+int state;
+pid_t child;
+
+void detectarAlarme (int sig){
+  if(state==0)
+  {
+     kill(idProcesso,SIGWINCH);
+     alarm(3);
+  }
+  else if(state==1)
+  {
+     kill(idProcesso,SIGUSR1);
+     alarm(2);
+  }
+  else if(state==2)
+  {
+     kill(idProcesso,SIGUSR2);
+     alarm(4);
+  }
+
+  state=++state%3;
+} 
+
+void funcaoSignalHandler (int sig)          //jordao vc precisa mexer aqui
+{
+  switch(sig)
+  {
+    case SIGWINCH: printf("Vermelha\n"); break;
+    case SIGUSR1: printf("Amarela\n"); break;
+    case SIGUSR2: printf("Verde\n"); break;
+  }
+}
+
+void interruption (int sig)
+{
+   state=0;
+   detectarAlarme(SIGALRM);
+}
+
+void ignore(int sig){}
+
+void ctrlz(int sig)
+{
+  exit(0);
+}
+ 
+int main(int argc, char *argv[])
+{
+    state=0;
+    printf("Iniciando o programa\n");
+ 
+    signal(SIGWINCH, funcaoSignalHandler);
+    signal(SIGUSR1, funcaoSignalHandler);
+    signal(SIGUSR2, funcaoSignalHandler);
+    signal(SIGTSTP, ctrlz);
+ 
+    idProcesso = fork();
+    switch(idProcesso){
+      case -1:       
+        exit(1);
+      case 0:   
+        signal(SIGINT, ignore);
+        while(1){
+          sleep(1);
+        }
+        break;
+      default:  
+        signal(SIGINT, interruption);
+        signal(SIGALRM,detectarAlarme);
+        detectarAlarme(SIGALRM);
+        while(1){
+          sleep(1);
+        }
+        break;
+    }
+ 
+    return 0;
+}
